0b0a8a40ada66097f6286a9fa116fa04676a8293
Oscar Bernal <odbg2013@gmail.com>
2022-12-06 11:39:42 -0500

    add header list

--- social-network/src/components/Header/Header.js
+++ social-network/src/components/Header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
 export default function Header() {
-	return (
-		<div>
-			
-		</div>
-	)
+  return (
+    <div>
+      <ul>
+        <li id='icon-home'>
+          <span></span>
+        </li>
+        <li id='icon-trend'>
+          <span></span>
+        </li>
+        <li id='icon-explore'>
+          <span></span>
+        </li>
+        <li id='icon-categories'>
+          <span></span>
+        </li>
+        <li id='icon-profile'>
+          <span></span>
+        </li>
+      </ul>
+    </div>
+  )
 }
--- social-network/src/components/Home/Home.js
+++ social-network/src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../Header/Header.js'
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <button>+</button>
     </div>
   )
