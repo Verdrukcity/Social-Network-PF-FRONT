650dd256e406a40649abbbe3c196ef8755280757
Oscar Bernal <odbg2013@gmail.com>
2022-12-06 12:02:46 -0500

    add commentaries and fix typo

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -2,6 +2,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home/Home'
 
+/*
+  App se utiliza para navegar a las rutas necesarias en el frontend:
+   • Home en la ruta '/'
+
+   nota: se usa react-router-dom 5.2.0
+*/
+
 function App() {
   return (
     <BrowserRouter>
