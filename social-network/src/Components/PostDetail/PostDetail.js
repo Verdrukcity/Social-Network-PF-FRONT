b3f147d821511fd72079657698c85e7178c5162c
unknown <rodrigarlati@gmail.com>
2022-12-11 16:54:13 -0300

    avances detail

--- social-network/src/Components/PostDetail/PostDetail.js
+++ social-network/src/Components/PostDetail/PostDetail.js
@@ -3,24 +3,50 @@ import './PostDetail.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { arrowUp } from "../../shared/assets/icons/all-icons";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from "react";
+import {getPostDetailAsync} from '../../redux/reducer/postsReducer'
 
 
 export default function PostDetail(props){
+    const [postDetail, setPostDetail] = useState({
+        profile: '',
+        profileImg: '',
+        description: '',
+        postImg: '',
+        categories: [],
+        comments: []
+    })
+    const dispatch = useDispatch() 
 
+    //obtenemos el id por params para buscar el detail correpondiente
     const id = props.match.params.id;
-    //dispatch de post
 
-    const perfil="UserName Prueba"
-    const descripcion="   Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ab ad in molestias consequatur"
-                        +" Nostrum, sint. Accusamus harum nisi beatae praesentium magnam temporibus atque incidunt eum esse aperiam." 
-                        +   " Perferendis, praesentium.";
-    const imagenPost="https://nextluxury.com/wp-content/uploads/halo-tv-series.png"
-    const categorias=["category 1","category 2","category 3","category 3"]    
-    const comentarios=[{usuario:"alberto123", imagen:"https://b.rgbimg.com/users/a/al/alfi007/300/sEPfDia.jpg", textoComentario:"magnifico"},
-                        {usuario:"Gonzalo96", imagen:"https://b.rgbimg.com/users/a/al/alfi007/300/sEPfDia.jpg", textoComentario:"perdi mi tiepo con este post "},
-                        {usuario:"Iclacreyo", imagen:"https://b.rgbimg.com/users/a/al/alfi007/300/sEPfDia.jpg", textoComentario:"gracias por la informacion"},
-                    {usuario:"Iclacreyo", imagen:"https://b.rgbimg.com/users/a/al/alfi007/300/sEPfDia.jpg", textoComentario:"gracias por la informacion"},
-                {usuario:"Iclacreyo", imagen:"https://b.rgbimg.com/users/a/al/alfi007/300/sEPfDia.jpg", textoComentario:"gracias por la informacion"},]
+    //dispatch para obtener el detalle
+    useEffect(()=>{
+        dispatch(getPostDetailAsync(id))
+    },[])
+
+    //traigo el detail del store
+    const details = useSelector(state => state.posts.detail)
+
+    
+    //cuando este la info en el reducer, la traigo al componente
+    useEffect(()=>{
+        
+        //este if es cochino, pero si no lo hacia me rompía, perdonchi
+        if (Object.keys(details).length){
+            
+            setPostDetail({
+                profile: details.userId[0].user_Name,
+                profileImg: details.userId[0].image_profil,
+                description: details.post.text,
+                postImg: details.post.multimedia,
+                categories: details.post.category,
+                comments: details.comment
+            })
+        }
+    }, [details])
 
     return(
         
@@ -30,12 +56,12 @@ export default function PostDetail(props){
                 
                 {/* primera columna con foto y cateogries */}
                 <div className="col-8">
-                    <img className="postImg" src={imagenPost}/>
+                    <img className="postImg" src={postDetail.postImg}/>
 
                     {/* Aqui va a mapear las categories que contenga el post (esto es temporal)*/}
                     <div className="categoriescont">
                          <p className="categorysimbol">#</p>
-                          { categorias.length!==0?categorias.map(                    
+                          { postDetail.categories.length!==0?postDetail.categories.map(                    
                         (categoria)=> <p className="category">{categoria}</p>
                         ):( <div><p >Sin categorias</p>
                           </div> )
@@ -49,34 +75,34 @@ export default function PostDetail(props){
                 {/* segunda columna con user, texto de post y comentarios */}
                 <div className="col userandcomments">
                     <div className="row">
-                        <img className="col-2 userimg" src="https://b.rgbimg.com/users/a/al/alfi007/300/sEPfDia.jpg"></img>
-                        <h3 className="col username">{perfil} {id}</h3>
+                        <img className="col-2 userimg" src={postDetail.profileImg}></img>
+                        <h3 className="col username">{postDetail.profile}</h3>
                     </div>
                     <div className="textpost">
-                        <p className="row">{descripcion}
-                          
-                        </p>
+                        <p className="row">{postDetail.description}</p>
                     </div>
 
-                    
-                    <div className="listaComentarios">
+                    <h4>Coments</h4>
+                    <div className="comentscont">
+                        
                         {/* Aqui va a mapear los comentarios (esto es temporal) */}
-
-                        { comentarios.length!==0?comentarios.map(                    
-                        (comentario)=> { return(
-                              <div className="textComent">
-                                <div className="row">
-                                    <img className="col-2 userimg" src={comentario.imagen}></img>
-                                    <p className="">{comentario.usuario}</p>
+                        
+                        { postDetail.comments.length!==0?postDetail.comments.map(                    
+                        (comment)=> { return(
+                                <div className="textComent">
+                                    <div className="row">
+                                        <img className="col-2 userimg" src={comment.user.image_profil}></img>
+                                        <p className="col">{comment.user.user_Name}</p>
+                                    </div> 
+                            
+                                    <p className="">{comment.comment.text}</p>
+                            
                                 </div> 
-                            
-                                <p className="">{comentario.textoComentario}</p>
-                            
-                        </div> 
-                        )                    
+                                )                    
                         }
                         ):( <p >Sin comentarios</p>
-                           )  } 
+                        )  } 
+                    
                     </div>
                 </div>
 
