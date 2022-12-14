df0d340ef6daa8b9cc4ab0bd8209bf82db66897f
unknown <rodrigarlati@gmail.com>
2022-12-14 17:43:42 -0300

    Se puede crear usuario del front, no terminado

--- social-network/src/redux/actions/commentsActions.js
+++ social-network/src/redux/actions/commentsActions.js
@@ -1,10 +1,13 @@
 import axios from "axios"
+import { postComment } from "../reducer/commentsReducer"
 
 export const createComment = (data, postId) => {
-    return async function () {
+    return async function (dispatch) {
         try {
-            await axios.post(`http://127.0.0.1:3001/comment/${postId}`, data)
+            const commentStatus = await axios.post(`http://127.0.0.1:3001/comment/${postId}`, data)
             //console.log('createcomment')
+            console.log(commentStatus)
+            dispatch(postComment(commentStatus))
         } catch (error) {
             console.log(error)
         }
--- social-network/src/redux/actions/countriesActions.js
+++ social-network/src/redux/actions/countriesActions.js
@@ -6,9 +6,25 @@ export const getAllCountriesAsync = () => async (dispatch) => {
      * TODO: consultar endpoint con todos los países
      */
     try {
-        const response = await axios.get('https://restcountries.com/v3/all')
+        const response = await axios.get('http://127.0.0.1:3001/maps')
 
-        const countriesName = response.data.map((country) => country.name.common)
+        //por cada pais un objeto con nombre e id para buscar en la db
+        let countriesName = response.data.data.map((country) => ({
+            name: country.name,
+            id: country._id
+        }))
+        
+        //ordena alfabeticamente
+        countriesName = countriesName.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0
+        })
+        
 
         dispatch(getAllCountries(countriesName))
     } catch (error) {
