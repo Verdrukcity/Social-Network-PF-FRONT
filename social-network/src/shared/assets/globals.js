2ecb44f0d5c5ad3d0e43e031b9ac900d68d94d4e
Oscar Bernal <odbg2013@gmail.com>
2022-12-30 19:55:07 -0500

    add dialog searchBar also added responsive on categories dialog

--- social-network/src/shared/assets/globals.js
+++ social-network/src/shared/assets/globals.js
@@ -9,16 +9,44 @@ export const activeRoundedButton = {
 export const categoryBox = {
 	position: 'absolute',
 	top: 50,
-	right: -160,
+	right: -100,
 	zIndex: 1,
 	border: 'none',
 	borderRadius: '25px',
 	p: 1,
-	padding: '15px 70px',
-	width: '600px',
+	padding: '10px 30px',
+	width: '400px',
 	height: 'auto',
 	textAlign: 'left',
 	backgroundColor: '#D9D9D9',
 	boxShadow:
 		'rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px',
+	['@media (max-width:780px)']: {
+		width: '400px',
+		padding: '10px 30px',
+		left: -260,
+	},
+	['@media (min-width:1200px)']: {
+		width: '600px',
+		padding: '15px 75px',
+		left: -400,
+	},
+}
+
+export const searchBox = {
+	position: 'absolute',
+	top: 50,
+	left: -280,
+	borderRadius: '25px',
+	padding: '10px 40px',
+	width: '600px',
+	height: 'auto',
+	textAlign: 'center',
+	backgroundColor: '#D9D9D9',
+	boxShadow:
+		'rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px',
+	['@media (max-width:1200px)']: {
+		left: -175,
+		width: '400px',
+	},
 }
