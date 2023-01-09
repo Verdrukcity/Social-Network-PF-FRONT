0b15d11b011625c19ce56e7f2ad0b6e4c66bbc5d
brstevenn <brayangaitan81@gmail.com>
2023-01-09 11:48:59 -0500

    Add admin acctions and reducer

--- social-network/src/redux/actions/adminActions.js
+++ social-network/src/redux/actions/adminActions.js
@@ -2,12 +2,30 @@ import axios from 'axios'
 import {
 	getAllUsersVerify,
 } from '../reducer/adminReducer'
-
+import {getAllPostsAsync} from './postActions.js'
 
 export const getAllUsersVerifyAsync = (token) => async (dispatch) => {
 	try {
 		const response = await axios.get(`/usersverify?token=${token}`)
 		dispatch(getAllUsersVerify(response.data))
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+export const updateUserStatus = (id, token) => async (dispatch) => {
+	try {
+		const response = await axios.post(`/admin?token=${token}`, {id: id})
+		dispatch(getAllUsersVerifyAsync(token))
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+export const updatePostStatus = (id, token) => async (dispatch) => {
+	try {
+		const response = await axios.post(`/admin/post?token=${token}`, {id: id})
+		dispatch(getAllPostsAsync(token))
 	} catch (error) {
 		console.log(error)
 	}
